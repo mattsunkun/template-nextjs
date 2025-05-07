44e28eab99724dfd9f0bc00ee9deb4c612688ae6
mattsunkun <mattsunkun1221@gmail.com>
2025-05-07 21:04:09 +0900

    action-fixed2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir: 'dist'
-};
+    basePath: "/template-nextjs",
+    output: "export",
+    images: { unoptimized: true } ,
+}
+
+// const nextConfig = {
+//     output: 'export',
+//     distDir: 'dist'
+// };
 
 export default nextConfig;
